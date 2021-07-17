a460067e509830bd3ac75f5e80a20bbba05443f5
Jono <jkwok881@gmail.com>
2021-07-18 09:58:29 +1000

    centre aligned titles

--- src/pages/Detect/Detect.jsx
+++ src/pages/Detect/Detect.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ObjectDetector } from '../../components/ObjectDetector/ObjectDetector';
 import Header from '../../components/Header/Header';
+import './Detect.css';
 
 export default function Detect() {
   return (
     <div>
       <Header header="Detect Image"/>
-      <h1>Start learning by detecting an object!!!!!</h1>
+      <h1 className="title-centre-align">Start learning by detecting an object!</h1>
       <ObjectDetector/>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
+import './Home.css';
 
 export default function Home() {
     return (
         <div>
             <Header header="Home"/>
-            <p>Home page body content</p>
+            <h1 className="title-centre-align">World's First Mandarin Language Learning App for Dyslexia</h1>
         </div>
     )
 }
