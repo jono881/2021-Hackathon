457cd43786e00024001ae1ab4d329c60f7dc8089
Jono <jkwok881@gmail.com>
2021-07-18 11:02:13 +1000

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import './Home.css';
+import {FaCamera, FaBook, FaSearch} from 'react-icons/fa';
+
 
 export default function Home() {
     return (
-        <div>
+        <section>
             <Header header="Home"/>
             <h1 className="title-centre-align">World's First Mandarin Language Learning App for Dyslexia</h1>
-            <p className="title-left-align">Image Detection</p>
-        </div>
+
+            <div className="text-left">
+                <h2>Features</h2>
+                <h3>Image Detection</h3>
+                {/* <FaCamera size="100" className="image-left"/> */}
+                <p>Select an image from your device and our web application translates the object in the image to produce a written translation in your desired language.</p>
+
+                <h3>Flashcards</h3>
+                {/* <FaCamera size="100" className="image-left"/> */}
+                <p>Practise remembering key words saved from the image detection feature and pronouncing them correctly with an audio clip.</p>
+
+                <h3>Search</h3>
+                {/* <FaCamera size="100" className="image-left"/> */}
+                <p>Find certain flashcards containing key words to assist with your practising.</p>
+
+            </div>
+            {/* <FaCamera size="100" className="image-left"/>
+            <FaBook size="50"/>
+            <FaSearch size="50"/> */}
+
+        </section>
+
     )
 }
